3a52b7bc449e02133be72666372aeee6a88847fe
Leandro Pereira <leandro.nares@gmail.com>
2024-07-12 13:02:10 -0300

    Chapter 9.3: Step Definition Files

---
+++ features/step_definitions/stepdefs.js
@@ -0,0 +1,52 @@
+const assert = require('assert');
+const { Given, When, Then } = require('@cucumber/cucumber');
+
+const URL = 'https://demo.nopcommerce.com/register';
+
+Given('I open the registration page', function () {
+
+});
+
+When('I select the gender', function () {
+
+});
+
+When('I enter First Name {string}', function (firstname) {
+
+});
+
+When('I enter Last Name {string}', function (lastname) {
+
+});
+
+When('I select Date of Birth {string}', function (day) {
+
+});
+
+When('I select Month of Birth {string}', function (month) {
+
+});
+
+When('I select Year of Birth {string}', function (year) {
+
+});
+
+When('I enter Email {string}', function (email) {
+
+});
+
+When('I enter Password of Birth {string}', function (password) {
+
+});
+
+When('I enter Confirm Password {string}', function (password) {
+
+});
+
+When('I click register button', function () {
+
+});
+
+Then('successfull message is displayed', function () {
+
+});
---
+++ pages/RegisterPage.js
@@ -0,0 +1,44 @@
+const { Selector} = require ('testcafe');
+
+function select(select){
+    return Selector(selector).with({boundTestRun:testController})
+}
+
+exports.RegisterPage = {
+    GenderButton: function(){
+        return select('#gender-male')
+    },
+    Firstname: function(){
+        return select('#FirstName')
+    },
+    Lastname: function(){
+        return select('#LastName')
+    },
+    DateOfBirth: function(){
+        return select("select[name='DateOfBirthDay']")
+    },
+    DateOfMonth: function(){
+        return select("select[name='DateOfBirthMonth']")
+    },
+    DateOfYear: function(){
+        return select("select[name='DateOfBirthYear']")
+    },
+    Email: function(){
+        return select('#Email')
+    },
+    Password: function(){
+        return select('#Password')
+    },
+    ConfirmPassword: function(){
+        return select('#ConfirmPassword')
+    },
+    RegistrationButton: function(){
+        return select('#register-button')
+    },
+    SuccessfulMessage: function(){
+        return select('div.result').withText('Your registration completed')
+    },
+    ListOption: function(){
+        return select('option')
+    }
+}
